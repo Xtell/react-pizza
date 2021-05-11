081a90563686631737fda523e909191bcfbdde14
Andrey Evstafyev <ean@d-element.ru>
2021-05-11 18:13:33 +0500

    add redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import {BrowserRouter as Router} from 'react-router-dom'
 import './scss/app.scss';
 import App from './App';
